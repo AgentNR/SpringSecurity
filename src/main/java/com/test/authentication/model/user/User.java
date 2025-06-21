06100f27021acf2b87556bd1cb201e1343ba6d98
Nipuna Rajapaksa <rajapaksanipuna@gmail.com>
2025-06-21 14:03:00 +0530

    created User class

--- src/main/java/com/test/authentication/model/user/User.java
+++ src/main/java/com/test/authentication/model/user/User.java
@@ -1,4 +1,22 @@
 package com.test.authentication.model.user;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "user")
 public class User {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
